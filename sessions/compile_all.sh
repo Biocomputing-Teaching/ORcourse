6172be98e6e199e5eaaddd14bdd32050340b9487
JordiVillaFreixa <jordi.villa@uvic.cat>
2023-02-21 15:00:27 +0100

    unit1 NLO

--- sessions/compile_all.sh
+++ sessions/compile_all.sh
@@ -1,13 +1,14 @@
 # ara mateix preparat per al curs 22-23, on he eliminat les simulacions estocàstiques i Markov
 
 for file in \
-        Unit1_Introduction \
+        Unit1_Introduction_Nonlinear \
         ; do
 #            lualatex $file.tex
         lualatex $file.tex
 done
 
-:'            Unit1_Introduction_Nonlinear \
+:'Unit1_Introduction \
+        Unit1_Introduction_Nonlinear \
 #            Unit2_LinearProgramming_Introduction \
 #            Unit2_LinearProgramming_Simplex \
 #            Unit2_LinearProgramming_Duality \
